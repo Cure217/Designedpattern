2c638e2b4ac702b830f934c0650a8159e6c46bf3
Cure <1318822416@qq.com>
2022-08-31 23:15:40 +0800

    第一次commit

---
+++ src/main/java/com/aliano/gitstudy/GitstudyApplication.java
@@ -0,0 +1,13 @@
+package com.aliano.gitstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitstudyApplication.class, args);
+    }
+
+}
